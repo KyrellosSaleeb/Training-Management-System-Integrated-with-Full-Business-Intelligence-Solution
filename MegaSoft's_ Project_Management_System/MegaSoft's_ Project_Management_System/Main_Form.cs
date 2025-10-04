f6ef2539a6974a21596dc5484809e053f2a3110b
KyrellosSaleeb <kyrellossaleeb32@gmail.com>
2025-10-04 16:07:23 +0300

    Initial commit

---
+++ MegaSoft's_ Project_Management_System/MegaSoft's_ Project_Management_System/Main_Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaSoft_s__Project_Management_System
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuFormDock1_FormDragging(object sender, Bunifu.UI.WinForms.Renderers.Snackbar.BunifuFormDock.FormDraggingEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MegaSoft's_ Project_Management_System/MegaSoft_PMS/MainForm_MS0.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaSoft_PMS
+{
+    public partial class UserControl1: UserControl
+    {
+
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void panel_sidebar(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Students(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Students_Btn(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Registration(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Manage_Students(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void print_student(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_Logo(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Course(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Add_course(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Edit_course(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Print_course(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Score(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Add_Score(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void edit_Score(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Print_Score(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
